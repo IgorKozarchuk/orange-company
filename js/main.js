4d178ce98f294841498dba29a1bd7b6fae098d1f
IgorKozarchuk <igor.a.kozarchuk@gmail.com>
2017-11-07 19:48:26 +0200

    finished

--- js/main.js
+++ js/main.js
@@ -1,25 +1,51 @@
-// Multi-item Carousel
-// https://codepen.io/mephysto/pen/ZYVKRY
-
-// instantiate the Bootstrap carousel
-$('.multi-item-carousel').carousel({
-	interval: false
-});
-
-// for every slide in carousel, copy the next slide's item in the slide.
-// do the same for the next, next item.
-$('.multi-item-carousel .item').each(function() {
-	var next = $(this).next();
+$(function() {
 	
-	if (!next.length) {
-		next = $(this).siblings(':first');
-	}
-	
-	next.children(':first-child').clone().appendTo($(this));
+	// Smooth scrolling
+	$('.navbar.navbar-default.navbar-fixed-top a, footer .btn-top').on('click', function(event) {
+		if (this.hash !== "") {
+			event.preventDefault();
+			var hash = this.hash;
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 800, function() {
+				window.location.hash = hash;
+			});
+		}
+	});
 
-	if (next.next().length > 0) {
-		next.next().children(':first-child').clone().appendTo($(this));
-	} else {
-		$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-	}
+	// Multi-item Carousel
+	// https://codepen.io/mephysto/pen/ZYVKRY
+	// 1) instantiate the Bootstrap carousel
+	$('.multi-item-carousel').carousel({
+		interval: false
+	});
+	// 2) for every slide in carousel, copy the next slide's item in the slide
+	// 3 ) do the same for the next, next item
+	$('.multi-item-carousel .item').each(function() {
+		var next = $(this).next();
+		
+		if (!next.length) {
+			next = $(this).siblings(':first');
+		}
+		
+		next.children(':first-child').clone().appendTo($(this));
+
+		if (next.next().length > 0) {
+			next.next().children(':first-child').clone().appendTo($(this));
+		} else {
+			$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+		}
+	});
+
+	// Slide in animation
+	$(window).scroll(function() {
+		$('.slidein').each(function() {
+			var elemPos = $(this).offset().top;
+			var scrollPos = $(window).scrollTop();
+			if (elemPos < scrollPos + 600) {
+				$(this).addClass('slide-item');
+			}
+		});
+	});
+
 });
